c0dd116996766f06070ee457b253449a88e0703b
Victoriia <viktoriia.paster@beetroot.se>
2025-05-19 19:13:03 +0300

    ADD | basic layout

---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+import styles from "./Logo.module.css";
+import { Link } from "react-router-dom";
+
+export function Logo() {
+  return <Link to="/"><img src="/logo.png" alt="WorldWise logo" className={styles.logo} /></Link>
+}
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import { AppNav } from "../components/AppNav"
+
+export function AppLayout() {
+    return (
+        <div>
+            <AppNav />
+            <h1>App Layout</h1>
+        </div>
+    )
+}
